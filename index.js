909b3cb9449a674809a040ce20887e5d10612617
Terry Blue <terryblue@comcast.net>
2020-02-16 22:58:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const batteries = () => {
-  return totalBatteries = batteryBatches.reduce((total, element) => element + total, 0)
+  let totalBatteries = batteryBatches.reduce((total, element) => element + total, 0)
+  return totalBatteries
 }
